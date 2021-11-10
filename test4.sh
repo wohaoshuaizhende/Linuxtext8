f3fe85a3ded4fdf54fe719220e99e29f87bf8bf6
cry <59107840@qq.com>
2021-11-10 22:10:44 +0800

    123

---
+++ test4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read name animal1 animal2 animal3
+animal1=panda
+animal2=lion
+animal3=duck
+name=pengyuan
+echo "welcome to our ${name} zoo"
+echo "we have ${animal1} ${animal2} ${animal3}"
---
+++ test5.sh
@@ -0,0 +1,7 @@
+#i/bin/bash
+sum=0
+for((i=0;i<100;i++))
+do
+  ((sum += i))
+done
+echo $sum
